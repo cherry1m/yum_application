809509d255df457885b18d9371f7eddfe8598259
formart12 <formart12@naver.com>
2024-10-29 21:19:09 +0900

    테마 수정, 날짜 다이얼로그 추가

--- lib/main.dart
+++ lib/main.dart
@@ -16,10 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: light,
       darkTheme: dark,
-      home: const Scaffold(
-        backgroundColor: Colors.black,
-        body: Center(child: HomeView()),
-      ),
+      home: const HomeView(),
     );
   }
 }
