52dbf6076299f23e6e1e63821c34c0559561fa38
cherry1m <ini_045@naver.com>
2024-11-05 21:06:17 +0900

    재료 수정 바텀 시트 완성

--- lib/src/ingredient/widget/ingredient_edit_bottom_sheet.dart
+++ lib/src/ingredient/widget/ingredient_edit_bottom_sheet.dart
@@ -59,16 +59,20 @@ class _IngredientEditBottomSheetState extends State<IngredientEditBottomSheet> {
       ]);
 
   Widget _bottom() {
+    final ratio = MediaQuery.of(context).devicePixelRatio;
+    final height = 130 / ratio;
+    final width = height * (10 / 3);
     return Padding(
       padding: const EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 40),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           ElevatedButton(
             style: ElevatedButton.styleFrom(
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(24)),
               backgroundColor: Theme.of(context).colorScheme.primary,
-              fixedSize: const Size(173, 48),
+              fixedSize: Size(width, height),
             ),
             onPressed: () {},
             child: Text(
@@ -83,7 +87,7 @@ class _IngredientEditBottomSheetState extends State<IngredientEditBottomSheet> {
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(24)),
                 backgroundColor: Theme.of(context).colorScheme.primary,
-                fixedSize: const Size(173, 48),
+                fixedSize: Size(width, height),
               ),
               onPressed: () {},
               child: Text(
