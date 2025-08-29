c446fac2acdd1524da191dd8bbbd8eb291a12cda
sangwook16074 <qazws78941@gmail.com>
2025-08-29 11:56:41 +0900

    바텀네비게이션 뷰모델 제작 완료

---
+++ lib/src/features/app/di/app_di.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'app_di.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$bottomNavViewModelHash() =>
+    r'2f8a35eba2830dea5999a57a9176fe3e81f58c08';
+
+/// See also [bottomNavViewModel].
+@ProviderFor(bottomNavViewModel)
+final bottomNavViewModelProvider =
+    AutoDisposeProvider<BottomNavViewModel>.internal(
+  bottomNavViewModel,
+  name: r'bottomNavViewModelProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$bottomNavViewModelHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef BottomNavViewModelRef = AutoDisposeProviderRef<BottomNavViewModel>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
---
+++ lib/src/features/app/presentation/viewModels/bottom_nav_view_model.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'bottom_nav_view_model.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$bottomNavViewModelHash() =>
+    r'5a4dade196f95e38f2bb43ffce4514a45e92e28b';
+
+/// See also [BottomNavViewModel].
+@ProviderFor(BottomNavViewModel)
+final bottomNavViewModelProvider =
+    AutoDisposeNotifierProvider<BottomNavViewModel, BottomNavStatus>.internal(
+  BottomNavViewModel.new,
+  name: r'bottomNavViewModelProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$bottomNavViewModelHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$BottomNavViewModel = AutoDisposeNotifier<BottomNavStatus>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
